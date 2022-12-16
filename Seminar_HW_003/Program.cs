5bd52272c7c08a224afaaa3278d1f12aef50649c
IgorMarokhin <lawyer.mii222@gmail.com>
2022-12-16 20:03:10 +0300

    Домашнее задание. Решение задачи номер 3: Проверка введенного числа на четность (деление с остатком

---
+++ Seminar_HW_003/Program.cs
@@ -0,0 +1,10 @@
+﻿// Решение Задачи 6.
+
+Console.Write("Введите целое число: ");
+int userInput = int.Parse(Console.ReadLine() ?? "");
+
+if (userInput % 2 == 0)
+Console.WriteLine($"{userInput} -> является четным числом");
+
+else if (userInput != 0)
+Console.WriteLine($"{userInput} -> не является четным числом");
