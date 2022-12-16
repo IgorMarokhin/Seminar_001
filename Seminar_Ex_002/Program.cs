6153f58c835df36c1ae31700807010e65e8cef8b
IgorMarokhin <lawyer.mii222@gmail.com>
2022-12-16 13:41:50 +0300

    Добавлено решение второй задачи

---
+++ Seminar_Ex_002/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите целое число в диапозоне от 1 до 7: ");
+int number = int.Parse(Console.ReadLine() ?? "");
+if (number == 1)
+Console.WriteLine($"1 -> понедельник");
+else if (number == 2)
+Console.WriteLine($"2 -> вторник");
+else if (number == 3)
+Console.WriteLine($"3 -> среда");
+else if (number == 4)
+Console.WriteLine($"4 -> четверг");
+else if (number == 5)
+Console.WriteLine($"5 -> пятница");
+else if (number == 6)
+Console.WriteLine($"6 -> суббота");
+else if (number == 7)
+Console.WriteLine($"7 -> воскресенье");
