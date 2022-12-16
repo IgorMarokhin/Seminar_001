5b370536a24c871035bc8ac8576258443a76aa56
IgorMarokhin <lawyer.mii222@gmail.com>
2022-12-16 20:56:43 +0300

    Домашнее задание. Решение задачи номер 4: Вывод заданного числа, а также всех четных числел в диапозоне от 1 до заданного числа

---
+++ Seminar_HW_004/Program.cs
@@ -0,0 +1,14 @@
+﻿// Решение Задачи 8. 
+
+Console.Write("Введите целое число: ");
+int N = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write($"{N} -> ");
+
+for (int count = 2; count < N; count++)
+
+    if (count % 2 == 0)
+    
+        Console.Write($"{count}, ");
+
+Console.WriteLine();
